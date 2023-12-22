57c61e49f10eab935510bab73be55a489ca57c11
thomas collignon <thomas.collignon@saintmichelannecy.fr>
2023-12-22 11:59:04 +0100

    Ajoutez des fichiers projet.

---
+++ View/Magnus.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mega_grid_warhammer.View
+{
+    /// <summary>
+    /// Logique d'interaction pour Magnus.xaml
+    /// </summary>
+    public partial class Magnus : UserControl
+    {
+        public Magnus()
+        {
+            InitializeComponent();
+        }
+    }
+}
